57865b08a2898adb8c5f28452da28fe95847161e
sofiita18-ops <sofiita18@gmail.com>
2026-09-09 20:29:02 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -637,7 +637,7 @@ function mostrarIBANCopiado(
                             style="border-radius: 12px"
                             src="https://open.spotify.com/embed/playlist/${SPOTIFY_PLAYLIST_ID}?utm_source=generator&theme=0"
                             width="100%"
-                            height="352"
+                            height="152"
                             frameborder="0"
                             allowfullscreen=""
                             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
